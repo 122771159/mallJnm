25611cfb4fe90cfe47fd66d81dae2fd1fa1d5b7c
李金木 <122771159@qq.com>
2025-06-03 09:29:48 +0800

    更新

---
+++ src/main/java/com/jnm/mallJnm/mapper/OneSalesCustomerVOMapper.java
@@ -0,0 +1,8 @@
+package com.jnm.mallJnm.mapper;
+
+import com.jnm.mallJnm.model.vo.OneSaleCustomersVO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface OneSalesCustomerVOMapper extends AbstractJoinMapper<OneSaleCustomersVO> {
+}
---
+++ src/main/java/com/jnm/mallJnm/mapper/SalesCustomerMapper.java
@@ -0,0 +1,9 @@
+package com.jnm.mallJnm.mapper;
+
+import com.jnm.mallJnm.model.SalesCustomer;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface SalesCustomerMapper extends DefaultMapper<SalesCustomer> {
+
+}
